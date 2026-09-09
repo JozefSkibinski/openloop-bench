b38729ffac913c4102d42c9bf9489792f54c4ee3
Jozef Skibinski <jozefskibinski@allerims-mbp.east.wireless.uic.edu>
2026-09-09 15:09:37 -0500

    added basic echo server

---
+++ src/echoserver.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+#define PORT 8080
+
+
+
+
+int main(){
+    int amnt;
+
+    std::cout << "Loop amount: ";
+    std::cin >> amnt;
+
+    int opt = 1;
+    struct sockaddr_in address;
+
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    
+    if(server_fd < 0){
+        perror("Socket Failed");
+        exit(EXIT_FAILURE);
+    }
+
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);//Assign it to port 8080
+
+    if(::bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0){
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if(listen(server_fd, 3) < 0){
+        perror("Listen Failed");
+        exit(EXIT_FAILURE);
+    }
+
+    int i = 0;  
+
+    while(i < amnt){
+
+        int clientfd = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+
+        if(clientfd < 0){
+            perror("Accept Failure");
+            exit(EXIT_FAILURE);
+        }
+
+        ssize_t numByte = read(clientfd, buffer, sizeof(buffer));
+
+        if(numByte < 0){
+            perror("Read Error");
+            exit(EXIT_FAILURE);
+        }
+
+        std::cout.write(buffer, numByte);
+
+        if(write(clientfd, buffer, numByte) < 0){
+            perror("write failed");
+            exit(EXIT_FAILURE);
+        }
+        i++;
+        close(clientfd);
+    }
+    
+    
+    close(server_fd);
+
+
+    return 0;
+}
